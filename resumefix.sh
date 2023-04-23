206e6f0f7e45adddb3335509ad6ebebf59ee58d4
muesli4brekkies <110121045+muesli4brekkies@users.noreply.github.com>
2023-04-23 16:39:57 +0100

    Update resumefix.sh

--- resumefix.sh
+++ resumefix.sh
@@ -2,4 +2,4 @@
 /bin/systemctl restart python3-validity.service && /usr/lib/open-fprintd/resume.py
 # python3-validity and open-fprintd-resume don't play nice at the mo. This little script fixes it.
 # You may need a sleep between the commands depending on how quickly python3-validity restarts on your system
-# Works on :Thinkpad x270 with 138a:0097 Validity fingerprint sensor
+# Works on Thinkpad x270 with 138a:0097 Validity fingerprint sensor
