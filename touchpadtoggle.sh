aa89ae75277f26c70641f9ec8fcb0d99460de180
muesli4brekkies <110121045+muesli4brekkies@users.noreply.github.com>
2023-04-23 17:00:37 +0100

    Update touchpadtoggle.sh

--- touchpadtoggle.sh
+++ touchpadtoggle.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-# Toggle touchpad status
-# Using libinput and xinput
+#
+# A tiny script that toggles the touchpad.
+# Bind it to run with a key combo in oyur favourite WM or DE. I find alt+space convenient.
+# Very handy on one of my laptops that activates the touchpad when picked up, because it is so very bendy and cheap.
 
 # Use xinput list and do a search for touchpads. Then get the first one and get its name.
 device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o | head -n1)"
 
-# If it was activated disable it and if it wasn't disable it
+# Toggle it!
 [[ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" == "1" ]] &&
     xinput disable "$device" ||
     xinput enable "$device"
