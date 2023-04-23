792cc79ba300f52e77b48bb72a36068945bbfd9f
muesli4brekkies <110121045+muesli4brekkies@users.noreply.github.com>
2023-04-23 16:39:25 +0100

    Update pluginscript.sh

--- pluginscript.sh
+++ pluginscript.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -e
+# 
+# A script that sounds chimes on AC power being plugged in and out
+# Set running with your .xinitrc or favourite DE or WM
+
 prev=0
-sleep 6 # Little sleep so the charging sounds don't step on the login sound
+sleep 6 # Little sleep so the charging sounds don't step on any login sounds
 while : ; do
 status=$(cat /sys/class/power_supply/AC0/online)
 if [ $status -gt $prev ] ; then
-    mpv /usr/share/sounds/plugin.ogg 
+    mpv /usr/share/sounds/plugin.ogg # Change this path to your sounds
     dunstify "Charging!" -t 2000
 elif [ $status -lt $prev ] ; then 
-    mpv /usr/share/sounds/plugout.ogg
+    mpv /usr/share/sounds/plugout.ogg # As above
     dunstify "NOT Charging!" -t 2000 
 fi
 prev=$status
